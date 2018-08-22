17db4e39e3742e0c553e92eace92b459456383ed
bensonresseler <bresseler.vdab@gmail.com>
2018-08-22 09:48:15 +0200

    Eigen probeersel

---
+++ src/com/company/HTML.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class HTML {
+    private String titel;
+    private String [] paragraaf;
+
+    private String HTML;
+    public HTML(String titel) {
+        this.titel = titel;
+    }
+
+    public void addParagraaf(String s) {
+
+    }
+
+    public String getHTML() {
+
+        return HTML;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
-public class Main {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main { private static final String BESTANDSNAAM="tekst.html";
+    private static final String TITEL = "Mijn eerste HTML bestand";
+    private static final String[] PARAGRAFEN = {"De eerste paragraaf", "De tweede paragraaf"};
+
+    public static void main(String[] args) throws IOException {
+        HTML html = new HTML(TITEL);
+        for(String s: PARAGRAFEN){
+            html.addParagraaf(s);
+        }
+        try(PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(BESTANDSNAAM)))){
+            pw.print(html.getHTML());
+        }
+
     }
+
 }
