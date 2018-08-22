66ec8f74eea143b854c45d6ccc39c6b2b277bdf1
bensonresseler <bresseler.vdab@gmail.com>
2018-08-22 10:15:35 +0200

    Eigen probeersel (oplossing) - poging 2 met hulp van Jef.

--- src/com/company/HTML.java
+++ src/com/company/HTML.java
@@ -2,19 +2,27 @@ package com.company;
 
 public class HTML {
     private String titel;
-    private String [] paragraaf;
-
+    private String paragraaf = "";
     private String HTML;
+
     public HTML(String titel) {
         this.titel = titel;
     }
 
     public void addParagraaf(String s) {
-
+        this.paragraaf = this.paragraaf + "<p>" + s + "</p>\n";
     }
 
     public String getHTML() {
-
+        HTML = "<html>\n" +
+                "<head>\n" +
+                String.format("<title>%s</title>\n", titel) +
+                "</head>\n" +
+                "<body>\n" +
+                String.format("<h1>%s</h1>\n", titel) +
+                this.paragraaf +
+                "</body>\n" +
+                "</html>\n";
         return HTML;
     }
 }
